84429e2e263c98429cb4949165067b95e0605e44
Andrew Han <andrewhan24@gmail.com>
2024-07-30 14:49:25 -0400

    import the json agent initializer

--- api/index.py
+++ api/index.py
@@ -2,13 +2,13 @@ import os
 
 from flask import Flask
 from langchain.llms import OpenAI
-from langchain.agents import AgentExecutor
+from langchain.agents import create_json_chat_agent 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    agent = AgentExecutor(agent=None, tools=[], verbose=True)
+    agent = create_json_chat_agent(None, [], prompt=None)
     llm = OpenAI(temperature=0.9)
     text = "What would be a good company name a company that makes colorful socks?"
 
