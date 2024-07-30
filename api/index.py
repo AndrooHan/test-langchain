6aa6239bd36d65ef04b314aae754e96cf7aa43b2
Andrew Han <andrewhan24@gmail.com>
2024-07-30 14:46:00 -0400

    test using langchain.agents

--- api/index.py
+++ api/index.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask
 from langchain.llms import OpenAI
+from langchain.agents import AgentExecutor
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    agent = AgentExecutor(agent=None, tools=[], verbose=True)
     llm = OpenAI(temperature=0.9)
     text = "What would be a good company name a company that makes colorful socks?"
 
